1d78328bc9609b21519ae981c073b172fab3bf43
Ruben Cancino <rubencancino6@gmail.com>
2016-04-18 11:16:57 -0500

    Ajustes

--- www/indicadores/papelbi1/papelKpi.service.js
+++ www/indicadores/papelbi1/papelKpi.service.js
@@ -8,11 +8,48 @@
   PapelKpi.$inject = ['$resource'];
 
   function PapelKpi($resource) {
-    return $resource('http://localhost:8080/api/bi/papelKpis',
+    var res = $resource('http://localhost:8080/api/bi/papelKpis',
       null,
       {
         'findByCalendario': { method: 'GET',isArray:false}
       });
+
+    res.prototype.getDiferenciaVentas = function(argument){
+      switch (argument) {
+        case 'semanal':
+          return (this.ventaSemanal - this.ventaSemanalMeta);
+          break;
+        case 'mensual':
+          return (this.ventaMensual - this.ventaMensualMeta);
+          break;
+        case 'anual':
+          return (this.ventaAnual - this.ventaAnualMeta);
+          break;
+        default:
+          return 0.0
+          break;
+      }
+    };
+
+    res.prototype.getDesviacionVentas = function(argument){
+      switch (argument) {
+        case 'semanal':
+          return ( (this.getDiferenciaVentas(argument)/this.ventaSemanal) * 100);
+          break;
+        case 'mensual':
+          return ( (this.getDiferenciaVentas(argument)/this.ventaMensual) * 100);
+          break;
+        case 'anual':
+          return ( (this.getDiferenciaVentas(argument)/this.ventaAnual) * 100);
+          break;
+        default:
+          return 0.0
+          break;
+      }
+    };
+
+
+    return res;
     
   }
 
